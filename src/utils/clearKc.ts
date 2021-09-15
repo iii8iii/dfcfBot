6e1c009dffbdfe44f9ed498c65270e2649d59aff
OCY <zichengou@gmail.com>
2021-09-15 16:36:02 +0800

    重写，为截取数据添加重试操作

--- src/utils/clearKc.ts
+++ src/utils/clearKc.ts
@@ -1,5 +1,10 @@
 import { stockItem } from '../types';
 
+/**
+ * 删除科创版股票代码
+ * @param {stockItem[]} data 如[{c:'123456'}]
+ * @return {*}
+ */
 export function clearKc(data: stockItem[]) {
   if (data.length) {
     return data.filter((s: stockItem) => !iskc(s.c));
@@ -7,4 +12,8 @@ export function clearKc(data: stockItem[]) {
   return [];
 }
 
+/**
+ * 通过正则确认传进来的股票代码是不是属于科创版
+ * @param {string} code
+ */
 const iskc = (code: string) => /^688\d{3}$/.test(code);
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 export const time = Date.now;
+
 export const numDate = (num = 0) => {
   return dayjs()
     .subtract(num, 'day')
