b27d32377bc4b3ecf3fb6978e417df8c56cc9b27
OCY <zichengou@gmail.com>
2021-10-11 10:57:48 +0800

    689

--- src/utils/market.ts
+++ src/utils/market.ts
@@ -8,5 +8,6 @@ export const marketCode = (code: string) => {
   }
 };
 
+
 const issz = (code: string) => /^(000|001|002|003|300|301)\d{3}$/.test(code);
-const issh = (code: string) => /^(600|601|603|605|688)\d{3}$/.test(code);
+const issh = (code: string) => /^(600|601|603|605|688|689)\d{3}$/.test(code);
