2cda94262137b95b8092f3009c00917727b64704
OCY <zichengou@gmail.com>
2021-11-19 16:09:29 +0800

    del tp test

--- test/tpstocks.test.ts
+++
@@ -1,9 +0,0 @@
-import { getTpStocks } from '../src/index';
-
-describe('getTpStocks', () => {
-  it('show data', async () => {
-    const data = await getTpStocks();
-    // console.log(data);
-    expect(data).not.toBeNull();
-  });
-});
